d133d757c61b4cac148dd8a418c303619cceab89
Sakib Rahman <seu.sakib@gmail.com>
2022-03-23 15:27:38 +0600

    calculate price

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,11 +1,23 @@
 import React from "react";
-
+import "./Cart.css";
 const Cart = ({ cart }) => {
   // const { id, name, price } = props.cart;
+  // console.log(cart
+  let total = 0;
+  let shipping = 0;
+  for (const product of cart) {
+    total = total + product.price;
+    shipping = shipping + product.shipping;
+  }
+  const tax = (total * 0.1).toFixed(2);
   return (
-    <div>
+    <div className="cart">
       <h4>Order Summary</h4>
       <p>Selected Items: {cart.length}</p>
+      <p>Total Price: ${total}</p>
+      <p>Total Shipping: ${shipping}</p>
+      <p>Tax: ${tax}</p>
+      <h6>Grand Total:</h6>
     </div>
   );
 };
