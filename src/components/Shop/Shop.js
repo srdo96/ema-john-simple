d86e546cb8514b434db81a9a9cb0c5c89f977db1
Sakib Rahman <seu.sakib@gmail.com>
2022-04-25 00:09:51 +0600

    add page number button

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -10,6 +10,8 @@ const Shop = () => {
   const [products, setProducts] = useProducts();
   const [cart, setCart] = useState([]);
   const [pageCount, setPageCount] = useState(0);
+  const [page, setPage] = useState(0);
+  const [size, setSize] = useState(10);
 
   useEffect(() => {
     fetch("http://localhost:5000/count")
@@ -63,8 +65,22 @@ const Shop = () => {
         ))}
         <div className="pagination">
           {[...Array(pageCount).keys()].map((num) => (
-            <button>{num + 1}</button>
+            <button
+              className={page === num ? "selected" : ""}
+              onClick={() => setPage(num)}
+            >
+              {num + 1}
+            </button>
           ))}
+
+          <select onChange={(e) => setSize(e.target.value)}>
+            <option value="5">5</option>
+            <option value="10" selected>
+              10
+            </option>
+            <option value="15">15</option>
+            <option value="20">20</option>
+          </select>
         </div>
       </div>
       <div className="cart-container">
